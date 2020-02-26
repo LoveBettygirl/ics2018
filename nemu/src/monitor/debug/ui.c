f3c052744322e2b16933b093ed383e3dc9e323fe
tracer-ics2017 <tracer@njuics.org>
2020-02-26 11:54:17 +0800

    >  compile 1410576 user Linux user-virtual-machine 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  11:54:17 up  8:22,  1 user,  load average: 0.16, 0.31, 0.24 57ac0912896037e1309c1bcf512f3eff22098a56

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -37,6 +37,9 @@ static int cmd_q(char *args) {
 }
 
 static int cmd_help(char *args);
+static int cmd_si(char *args);
+static int cmd_info(char *args);
+static int cmd_x(char *args);
 
 static struct {
   char *name;
@@ -46,6 +49,9 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "Step one instruction exactly", cmd_si },
+  { "info", "Display informations about the program being debugged", cmd_info },
+  { "x", "Examine memory", cmd_x }
 
   /* TODO: Add more commands */
 
@@ -72,6 +78,114 @@ static int cmd_help(char *args) {
       }
     }
     printf("Unknown command '%s'\n", arg);
+  }
+  return 0;
+}
+
+static int cmd_si(char *args) {
+  /* extract the first argument */
+  char *arg = strtok(NULL, " ");
+  uint32_t n = 0;
+
+  if (arg == NULL) {
+    /* no argument given */
+    cpu_exec(1); // 1 is the default
+  }
+  else {
+    char *temp = strtok(NULL, " ");
+    if (temp == NULL) {
+      sscanf(arg, "%u", &n);
+      cpu_exec(n); // Execute n times
+    }
+    else {
+      printf("Syntax error: Too much arguments");
+      return -1;
+    }
+  }
+  return 0;
+}
+
+static int cmd_info(char *args) {
+  /* extract the first argument */
+  char *arg = strtok(NULL, " ");
+
+  if (arg == NULL) {
+    /* no argument given, show rules */
+    printf("\"info\" must be followed by the name of an info command.\n");
+    printf("List of info subcommands:\n\n");
+    printf("info r -- List of integer registers and their contents\n");
+    printf("info w -- Status of specified watchpoints (all watchpoints if no argument)\n");
+  }
+  else if (strcmp(arg, "r")) {
+    printf("eax\t0x%x\t%d\n", reg_l(R_EAX), reg_l(R_EAX));
+    printf("ecx\t0x%x\t%d\n", reg_l(R_ECX), reg_l(R_ECX));
+    printf("edx\t0x%x\t%d\n", reg_l(R_EDX), reg_l(R_EDX));
+    printf("ebx\t0x%x\t%d\n", reg_l(R_EBX), reg_l(R_EBX));
+    printf("esp\t0x%x\t%d\n", reg_l(R_ESP), reg_l(R_ESP));
+    printf("ebp\t0x%x\t%d\n", reg_l(R_EBP), reg_l(R_EBP));
+    printf("esi\t0x%x\t%d\n", reg_l(R_ESI), reg_l(R_ESI));
+    printf("edi\t0x%x\t%d\n", reg_l(R_EDI), reg_l(R_EDI));
+    printf("eip\t0x%x\t%d\n", cpu.eip, cpu.eip);
+    printf("ax\t0x%x\t%d\n", reg_w(R_AX), reg_w(R_AX));
+    printf("cx\t0x%x\t%d\n", reg_w(R_CX), reg_w(R_CX));
+    printf("dx\t0x%x\t%d\n", reg_w(R_DX), reg_w(R_DX));
+    printf("bx\t0x%x\t%d\n", reg_w(R_BX), reg_w(R_BX));
+    printf("sp\t0x%x\t%d\n", reg_w(R_SP), reg_w(R_SP));
+    printf("bp\t0x%x\t%d\n", reg_w(R_BP), reg_w(R_BP));
+    printf("si\t0x%x\t%d\n", reg_w(R_SI), reg_w(R_SI));
+    printf("di\t0x%x\t%d\n", reg_w(R_DI), reg_w(R_DI));
+    printf("al\t0x%x\t%d\n", reg_b(R_AL), reg_b(R_AL));
+    printf("cl\t0x%x\t%d\n", reg_b(R_CL), reg_b(R_CL));
+    printf("dl\t0x%x\t%d\n", reg_b(R_DL), reg_b(R_DL));
+    printf("bl\t0x%x\t%d\n", reg_b(R_BL), reg_b(R_BL));
+    printf("ah\t0x%x\t%d\n", reg_b(R_AH), reg_b(R_AH));
+    printf("ch\t0x%x\t%d\n", reg_b(R_CH), reg_b(R_CH));
+    printf("dh\t0x%x\t%d\n", reg_b(R_DH), reg_b(R_DH));
+    printf("bh\t0x%x\t%d\n", reg_b(R_BH), reg_b(R_BH));
+  }
+  else if (strcmp(arg, "w")) {
+    printf("To be implemented...");
+  }
+  else {
+  	printf("Undefined info command: \"%s\".  Try \"help info\".\n", arg);
+  }
+  return 0;
+}
+
+static int cmd_x(char *args) {
+  /* extract the first argument */
+  char *arg = strtok(NULL, " ");
+  uint32_t n = 0;
+
+  if (arg == NULL) {
+    /* no argument given */
+    printf("Argument required (starting display address).\n");
+  }
+  else {
+    char *temp1 = strtok(NULL, " ");
+    char *temp2 = strtok(NULL, " ");
+    uint32_t start_addr = 0;
+    uint32_t mem_data = 0;
+    if (temp2 == NULL) {
+      sscanf(arg, "%u", &n);
+      sscanf(temp1, "%x", &start_addr);
+      uint32_t i;
+      for (i = 0; i < n ; i++) {
+      	printf("0x%x:\t", start_addr);
+      	mem_data = vaddr_read(start_addr, 4);
+      	uint32_t d;
+      	for (d = 0; d < 4; d++) {
+      		printf("%02x ", (mem_data & 0x000000ff));
+      		mem_data >>= 8;
+      	}
+      	printf("\n");
+      	start_addr += 4;
+      }
+    }
+    else {
+      printf("Syntax error: Too much arguments");
+      return -1;
+    }
   }
   return 0;
 }
