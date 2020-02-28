6a2d65c19605ab0077c63931e206e67897a50729
tracer-ics2017 <tracer@njuics.org>
2020-02-29 02:30:07 +0800

    >  compile 1410576 user Linux user-virtual-machine 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  02:30:07 up 1 day, 17:46,  1 user,  load average: 0.26, 0.18, 0.14 bdb91f24afa754cea98a650efd3c626751606ce

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -96,8 +96,9 @@ static bool make_token(char *e) {
 
         if (rules[i].token_type != TK_NOTYPE) {
           tokens[nr_token].type = rules[i].token_type;
-          memset(tokens[nr_token].str, 0, sizeof(tokens[nr_token].str));
+          //memset(tokens[nr_token].str, 0, sizeof(tokens[nr_token].str));
           strncpy(tokens[nr_token].str, substr_start, substr_len);
+          tokens[nr_token].str[substr_len] = '\0';
           nr_token++;
         }
 
