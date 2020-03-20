b719f2ff6f700584193e549f406957ee7606ed18
tracer-ics2017 <tracer@njuics.org>
2020-03-21 02:58:11 +0800

    >  compile 1410576 user Linux user-virtual-machine 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  02:58:11 up 4 days, 15:47,  1 user,  load average: 0.03, 0.07, 0.08 3255eb01a8fd958a52f0f441bb32ddfd4ded07e7

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -15,38 +15,30 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
     case CC_O:
-      rtl_get_OF(&t0);
-      *dest = t0 ? 1 : 0;
-      break;
+      rtl_get_OF(dest); break;
     case CC_B:
-      rtl_get_CF(&t0);
-      *dest = t0 ? 1 : 0;
-      break;
+      rtl_get_CF(dest); break;
     case CC_E:
-      rtl_get_ZF(&t0);
-      *dest = t0 ? 1 : 0;
-      break;
+      rtl_get_ZF(dest); break;
     case CC_BE:
       rtl_get_CF(&t0);
       rtl_get_ZF(&t1);
-      rtl_or(&t1, &t0, &t1);
-      *dest = t1 ? 1 : 0;
+      rtl_or(dest, &t0, &t1);
       break;
     case CC_S:
-      rtl_get_SF(&t0);
-      *dest = t0 ? 1 : 0;
-      break;
+      rtl_get_SF(dest); break;
     case CC_L:
       rtl_get_SF(&t0);
       rtl_get_OF(&t1);
-      *dest = t0 != t1 ? 1 : 0;
+      rtl_xor(dest, &t0, &t1);
       break;
     case CC_LE:
       //TODO();
       rtl_get_SF(&t0);
       rtl_get_OF(&t1);
       rtl_get_ZF(&t2);
-      *dest = t2 || (t0 != t1) ? 1 : 0;
+      rtl_xor(&t3, &t0, &t1);
+      rtl_or(dest, &t2, &t3);
       break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
