e2ae11b8544398eb7b1e430ae7aae4d3ba7e2353
tracer-ics2017 <tracer@njuics.org>
2020-03-22 02:43:12 +0800

    >  compile 1410576 user Linux user-virtual-machine 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  02:43:12 up 5 days,  6:46,  1 user,  load average: 0.30, 0.25, 0.20 78ac2959810f4798e3d7a8476fcca585c14f3057

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -79,10 +79,16 @@ make_EHelper(cltd) {
 
 make_EHelper(cwtl) {
   if (decoding.is_operand_size_16) {
-    TODO();
+    //TODO();
+    rtl_lr(&t0, R_AL, 1);
+    rtl_sext(&t0, &t0, 1);
+    rtl_sr(R_AX, 2, &t0);
   }
   else {
-    TODO();
+    //TODO();
+    rtl_lr(&t0, R_AX, 2);
+    rtl_sext(&t0, &t0, 2);
+    rtl_sr(R_EAX, 4, &t0);
   }
 
   print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
