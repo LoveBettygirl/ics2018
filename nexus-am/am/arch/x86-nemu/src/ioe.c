e58f5c14a726cc6fd7933b6b58c660798325f2fb
tracer-ics2017 <tracer@njuics.org>
2020-03-25 19:27:38 +0800

    >  run 1410576 user Linux user-virtual-machine 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  19:27:38 up 5 days, 12:47,  1 user,  load average: 0.46, 0.35, 0.26 2ae834c1a93320514378f3ac5ed420c344d80d2

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -22,12 +22,12 @@ _Screen _screen = {
 extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
-  int width = x + w <= _screen.width ? w : _screen.width - x;
+  //int width = x + w <= _screen.width ? w : _screen.width - x;
   int i;
   for (i = y; i < _screen.height; i++) {
   	if(i > y + h) break;
-  	memcpy(&fb[i * _screen.width + x], pixels, width * sizeof(uint32_t));
-  	pixels += width;
+  	memcpy(&fb[i * _screen.width + x], pixels, w * sizeof(uint32_t));
+  	pixels += w;
   }
 }
 
