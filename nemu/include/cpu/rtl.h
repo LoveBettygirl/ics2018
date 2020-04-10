df26aea7b4b252d58d9bb1b7ab3884c1fd2eb277
tracer-ics2017 <tracer@njuics.org>
2020-04-10 23:41:11 +0800

    >  compile 1410576 user Linux user-virtual-machine 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  23:41:11 up 8 days,  2:18,  1 user,  load average: 1.30, 0.61, 0.27 c7ffec13b31746f085c276663395740aec179d83

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -55,11 +55,11 @@ static inline void rtl_div(rtlreg_t* q, rtlreg_t* r, const rtlreg_t* src1_hi, co
   asm volatile("div %4" : "=a"(*q), "=d"(*r) : "d"(*src1_hi), "a"(*src1_lo), "r"(*src2));
 }
 
-inline void rtl_idiv(rtlreg_t* q, rtlreg_t* r, const rtlreg_t* src1_hi, const rtlreg_t* src1_lo, const rtlreg_t* src2) {
+static inline void rtl_idiv(rtlreg_t* q, rtlreg_t* r, const rtlreg_t* src1_hi, const rtlreg_t* src1_lo, const rtlreg_t* src2) {
   asm volatile("idiv %4" : "=a"(*q), "=d"(*r) : "d"(*src1_hi), "a"(*src1_lo), "r"(*src2));
 }
 
-static inline void rtl_lm(rtlreg_t *dest, const rtlreg_t* addr, int len) {
+inline void rtl_lm(rtlreg_t *dest, const rtlreg_t* addr, int len) {
   *dest = vaddr_read(*addr, len);
 }
 
