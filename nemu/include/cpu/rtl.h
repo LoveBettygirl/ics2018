b8af41f3027c01b4ea69ab1540f3fb435a5764c1
tracer-ics2017 <tracer@njuics.org>
2020-04-10 22:31:13 +0800

    >  compile 1410576 user Linux user-virtual-machine 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  22:31:13 up 8 days,  1:08,  1 user,  load average: 0.15, 0.07, 0.08 60fab5326e95e7a42b59420595b5bcadae37106

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -136,7 +136,7 @@ static inline void rtl_not(rtlreg_t* dest) {
   *dest = ~*dest;
 }
 
-inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
+static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   //TODO();
   switch(width) {
