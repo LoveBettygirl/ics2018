4dc4458b1d201938cfd3feff21aed7bc91e6db2e
tracer-ics2017 <tracer@njuics.org>
2020-04-10 23:47:22 +0800

    >  compile 1410576 user Linux user-virtual-machine 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  23:47:22 up 8 days,  2:25,  1 user,  load average: 0.49, 0.49, 0.31 bee58a77c7fee5b76ba99909768087b1c49c2

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -136,7 +136,7 @@ static inline void rtl_not(rtlreg_t* dest) {
   *dest = ~*dest;
 }
 
-inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
+static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   //TODO();
   switch(width) {
@@ -163,7 +163,7 @@ static inline void rtl_pop(rtlreg_t* dest) {
   rtl_addi(&cpu.esp, &cpu.esp, 4);
 }
 
-static inline void rtl_eq0(rtlreg_t* dest, const rtlreg_t* src1) {
+inline void rtl_eq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 == 0 ? 1 : 0)
   //TODO();
   *dest = *src1 == tzero ? 1 : 0;
