369df737a5735c87d68d571cfb81ad52b26d92ba
tracer-ics2017 <tracer@njuics.org>
2020-04-10 22:42:36 +0800

    >  compile 1410576 user Linux user-virtual-machine 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  22:42:36 up 8 days,  1:20,  1 user,  load average: 1.29, 0.79, 0.41 73fc091aabbe0aef17018393603df0df713be9b

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -182,7 +182,7 @@ static inline void rtl_neq0(rtlreg_t* dest, const rtlreg_t* src1) {
 }
 
 // get the sign bit
-static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
+static void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- src1[width * 8 - 1]
   //TODO();
   rtl_shri(dest, src1, width * 8 - 1);
