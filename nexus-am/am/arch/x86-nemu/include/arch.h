e5b1d19dd5c6d30c3117355be0f50a65cb68a12e
tracer-ics2017 <tracer@njuics.org>
2020-04-30 13:25:33 +0800

    >  run 1410576 user Linux user-virtual-machine 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  13:25:33 up 10 days, 58 min,  1 user,  load average: 0.47, 0.30, 0.18 72679b7b5dbfdf2b4c46b0c4f46702085c8c9673

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -7,8 +7,9 @@
 #define PGSIZE    4096    // Bytes mapped by a page
 
 struct _RegSet {
-  uintptr_t esi, ebx, eax, eip, edx, error_code, eflags, ecx, cs, esp, edi, ebp;
+  uintptr_t eflags, cs, eip, error_code;
   int       irq;
+  uintptr_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 };
 
 #define SYSCALL_ARG1(r) 0
