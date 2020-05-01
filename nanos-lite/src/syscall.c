78694d82fbbcb9b20d63af4133e158969b33441c
tracer-ics2017 <tracer@njuics.org>
2020-05-02 03:16:54 +0800

    >  run 1410576 user Linux user-virtual-machine 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  03:16:54 up 10 days,  3:43,  1 user,  load average: 0.15, 0.22, 0.25 c95ab5672ba021bd493809e4e80ff396a2d90e7

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,6 +11,7 @@ static uintptr_t sys_exit(int status) {
 }
 
 static uintptr_t sys_write(int fd, void *buf, size_t count) {
+	Log("");
 	if (fd == 1 || fd == 2) {
 		size_t i;
 		for (i = 0; i < count; i++) {
