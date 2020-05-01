1637959bf19b5dc096b0d6811450611a7b13ca4d
tracer-ics2017 <tracer@njuics.org>
2020-05-02 03:11:17 +0800

    >  run 1410576 user Linux user-virtual-machine 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  03:11:17 up 10 days,  3:37,  1 user,  load average: 0.48, 0.25, 0.27 8ddd649ef14a5e41ad509489894ec75f82763d9b

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,16 +1,23 @@
 #include "common.h"
 #include "syscall.h"
 
-static uintptr_t sys_write(_RegSet *r, int fd, void *buf, size_t count) {
+static uintptr_t sys_none() {
+	return 1;
+}
+
+static uintptr_t sys_exit(int status) {
+	_halt(status);
+	return status;
+}
+
+static uintptr_t sys_write(int fd, void *buf, size_t count) {
 	if (fd == 1 || fd == 2) {
 		size_t i;
 		for (i = 0; i < count; i++) {
 			_putc(*((char*)buf + i));
 		}
-		SYSCALL_ARG1(r) = i;
 		return i;
 	}
-	SYSCALL_ARG1(r) = -1;
 	return -1;
 }
 
@@ -22,11 +29,12 @@ _RegSet* do_syscall(_RegSet *r) {
   a[3] = SYSCALL_ARG4(r);
 
   switch (a[0]) {
-  	case SYS_none: SYSCALL_ARG1(r) = 1; break;
-  	case SYS_exit: _halt(a[1]); break;
-  	case SYS_write: sys_write(r, a[1], (void*)a[2], a[3]); break;
+  	case SYS_none: a[0] = sys_none(); break;
+  	case SYS_exit: a[0] = sys_exit(a[1]); break;
+  	case SYS_write: a[0] = sys_write(a[1], (void*)a[2], a[3]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+  SYSCALL_ARG1(r) = a[0];
 
   return NULL;
 }
