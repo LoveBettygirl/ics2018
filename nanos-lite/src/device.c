8909d7535677c6a501d591728cc76708cd297488
tracer-ics2017 <tracer@njuics.org>
2020-05-03 01:23:45 +0800

    >  run 1410576 user Linux user-virtual-machine 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  01:23:45 up 10 days, 18:15,  1 user,  load average: 0.16, 0.28, 0.34 faf55863cb61e6f3d12be4f27b0348d64eec6e6

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -22,17 +22,19 @@ void fb_write(const void *buf, off_t offset, size_t len) {
   off_t x = offset / sizeof(uint32_t) % _screen.width;
   off_t y = offset / sizeof(uint32_t) / _screen.width;
   off_t w = len / sizeof(uint32_t);
-  if (w >= _screen.width - x){
+  if (w >= _screen.width - x) {
   	_draw_rect(buf, x, y, _screen.width - 1 - x, 0);
   	off_t temp;
   	int i;
+  	uint32_t *buf1 = (uint32_t*)buf + (_screen.width - 1 - x);
   	temp = w = w - (_screen.width - 1 - x);
   	for (i = 1; i <= temp / (_screen.width - 1); i++) {
-  	  _draw_rect(buf, 0, y + i, _screen.width - 1, 0);
+  	  _draw_rect(buf1, 0, y + i, _screen.width - 1, 0);
   	  w -= (_screen.width - 1);
+  	  buf1 += (_screen.width - 1);
   	}
   	if (w != 0)
-  	  _draw_rect(buf, 0, y + i, w, 0);
+  	  _draw_rect(buf1, 0, y + i, w, 0);
   }
   else {
 	_draw_rect(buf, x, y, w, 0);
