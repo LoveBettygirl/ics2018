cbdc7a0b18f3210dc276b2bcfefe15dab5ca46e9
tracer-ics2017 <tracer@njuics.org>
2020-05-02 23:26:25 +0800

    >  run 1410576 user Linux user-virtual-machine 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  23:26:25 up 10 days, 16:18,  1 user,  load average: 0.41, 0.35, 0.23 2a4c60ccc51581156f5758b2427ec1f34389c555

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -38,7 +38,7 @@ void init_device() {
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
   int fd = fs_open("/proc/dispinfo", 0, 0);
-  char temp[128]={0};
+  char temp[130]={0};
   fs_read(fd, temp, fs_filesz(fd));
   Log("%s\n", temp);
   fs_close(fd);
