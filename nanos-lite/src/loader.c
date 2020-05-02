6fe7d62f6e4a020295ec2e0dc1f215fec632eef3
tracer-ics2017 <tracer@njuics.org>
2020-05-02 16:34:32 +0800

    >  run 1410576 user Linux user-virtual-machine 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  16:34:32 up 10 days,  9:26,  1 user,  load average: 0.72, 0.40, 0.30 fef572d3f4f19d55dbef6c3375358a4ec3465b4

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -7,12 +7,13 @@ extern size_t get_ramdisk_size();
 extern int fs_open(const char *pathname, int flags, int mode);
 extern ssize_t fs_read(int fd, void *buf, size_t len);
 extern int fs_close(int fd);
+extern size_t fs_filesz(int fd);
 
 uintptr_t loader(_Protect *as, const char *filename) {
   //TODO();
   //ramdisk_read((void*)DEFAULT_ENTRY, 0, get_ramdisk_size());
   int fd = fs_open(filename, 0, 0);
-  fs_read(fd, (void*)DEFAULT_ENTRY, get_ramdisk_size());
+  fs_read(fd, (void*)DEFAULT_ENTRY, fs_filesz(fd));
   fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
