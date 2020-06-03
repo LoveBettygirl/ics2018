df08d482defded65881fdf61f0fc51e68a7a08e2
tracer-ics2017 <tracer@njuics.org>
2020-06-03 15:44:28 +0800

    >  run 1410576 user Linux user-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  15:44:28 up  4:39,  1 user,  load average: 1.42, 1.02, 0.86 1a6c1803d482f1fe8ba9c33a2d78ab2a1aa3578

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -22,7 +22,6 @@ size_t events_read(void *buf, size_t len) {
     if (key == _KEY_F12 && down) {
       current_game = (current_game == 0 ? 2 : 0);
       Log("F12");
-      _trap();
     }
   }
   else {
