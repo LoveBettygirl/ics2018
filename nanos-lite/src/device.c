26c0046f11bdf17286ce270712047331d920b200
tracer-ics2017 <tracer@njuics.org>
2020-06-03 15:06:44 +0800

    >  run 1410576 user Linux user-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  15:06:44 up  4:01,  1 user,  load average: 0.99, 0.56, 0.30 577ddbdc1d22dee529e214c6c58f509539924901

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -20,7 +20,7 @@ size_t events_read(void *buf, size_t len) {
   if (key != _KEY_NONE) {
     snprintf((char*)buf, len + 1, "%s %s\n", down ? "kd" : "ku", keyname[key]);
     if (key == _KEY_F12) {
-      current_game = current_game == 0 ? 0 : 3;
+      current_game = current_game == 0 ? 0 : 2;
       _trap();
     }
   }
