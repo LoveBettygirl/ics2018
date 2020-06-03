5379e9332263fb52e1df1cc2670c611038ae59ee
tracer-ics2017 <tracer@njuics.org>
2020-06-03 11:31:55 +0800

    >  run 1410576 user Linux user-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  11:31:55 up  1:47,  1 user,  load average: 0.48, 0.89, 0.78 77525c7f2de022badb86f753a8990a4a61449c89

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,8 +5,8 @@ extern _RegSet* schedule(_RegSet *prev);
 
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
-  	case _EVENT_SYSCALL: do_syscall(r); break;
-  	case _EVENT_TRAP: Log("Event trap!"); return schedule(r);
+  	case _EVENT_SYSCALL: do_syscall(r); return schedule(r);
+  	case _EVENT_TRAP: return schedule(r);
     default: panic("Unhandled event ID = %d", e.event);
   }
 
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -29,7 +29,8 @@ void load_prog(const char *filename) {
 _RegSet* schedule(_RegSet *prev) {
   //return NULL;
   current->tf = prev;
-  current = &pcb[0];
+  //current = &pcb[0];
+  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   _switch(&current->as);
   return current->tf;
 }
