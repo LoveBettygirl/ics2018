ff952fd5d53abc786a7b33c713aeca28a2587ac3
tracer-ics2017 <tracer@njuics.org>
2020-06-17 14:20:25 +0800

    >  run 1410576 user Linux user-virtual-machine 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  14:20:25 up 1 day,  3:14,  1 user,  load average: 0.82, 0.85, 0.71 98ec5fa1e17f885d55c73db2f90a7b0dc30c6ad

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -19,7 +19,7 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
   //assert(0);
   //return 0;
   if (b == 0)
-    return 0x7f000000;
+    return -1;
   FLOAT ret = (a/b) << 16;
   if (((a ^ b) & 0x80000000) == 0x80000000) {
     ret |= 0x80000000;
